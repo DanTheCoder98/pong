e66200b1e7322dcb8e02db20fd5f8e13a5195040
Daniel Sarney <daniel_sarney@live.co.uk>
2024-10-27 19:03:21 +0000

    Right score

--- scoreboard.py
+++ scoreboard.py
@@ -11,3 +11,5 @@ class ScoreBoard(turtle.Turtle):
         self.right_score = 0
         self.goto(-100, 200)
         self.write(self.left_score, align="center", font=("Courier", 80, "normal"))
+        self.goto(100, 200)
+        self.write(self.right_score, align="center", font=("Courier", 80, "normal"))
